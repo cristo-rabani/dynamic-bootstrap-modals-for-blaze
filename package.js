e169abf3889b32fde07847efada21a05c400f2e5
Cristo <cristo.rabani@gmail.com>
2016-06-02 18:58:33 +0200

    Update package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cristo:dynamic-bootstrap-modals-for-blaze',
-  version: '1.0.0',
+  version: '1.0.1',
   // Brief, one-line summary of the package.
   summary: 'This package creates modals on the fly',
   // URL to the Git repository containing the source code for this package.
