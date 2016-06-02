0afb27677098ea78972c969e5305186736b82e9f
Kamil Wojdyło <kamil.wojdylo@vazco.eu>
2016-06-02 18:49:30 +0200

    Fixes searching and showing modal.

--- dynamic-modals-for-bootstrap.js
+++ dynamic-modals-for-bootstrap.js
@@ -9,29 +9,32 @@ export default function dynamicModal (template, context = {}, callbacks = {}) {
         throw new Error('You must pass blaze template');
     }
     template.onRendered(function () {
-        const $modal = this.$('.modal:first');
-        this.modal = $modal.modal.bind($modal);
+        Tracker.afterFlush(() => {
+            const $modal = $(this.firstNode);
+            this.modal = $modal.modal.bind($modal);
 
-        $modal.on('hidden.bs.modal', () => {
-            if (typeof hidden === 'function') {
-                hidden(this);
+            $modal.on('hidden.bs.modal', () => {
+                if (typeof hidden === 'function') {
+                    hidden(this);
+                }
+                Blaze.remove(this.view);
+            });
+
+            if (typeof hide === 'function') {
+                $modal.on('hide.bs.modal', () => hide(this));
             }
-            Blaze.remove(this.view);
+
+            if (typeof shown === 'function') {
+                $modal.on('shown.bs.modal', () => shown(this));
+            }
+
+            if (typeof show === 'function') {
+                $modal.on('show.bs.modal', () => show(this));
+            }
+            this.hide = () => this.modal('hide');
+            this.modal('show');
+
         });
-
-        if (typeof hide === 'function') {
-            $modal.on('hide.bs.modal', () => hide(this));
-        }
-
-        if (typeof shown === 'function') {
-            $modal.on('shown.bs.modal', () => shown(this));
-        }
-
-        if (typeof show === 'function') {
-            $modal.on('show.bs.modal', () => show(this));
-        }
-        this.hide = () => this.modal('hide');
-        this.modal('show');
     });
 
     return Blaze.renderWithData(template, context, document.body);
